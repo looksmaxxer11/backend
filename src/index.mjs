7fc226d38644245c0d1623001196a5b42711d80d
Karimov Elshod <karimovelshod4@gmail.com>
2024-12-16 19:28:24 +0500

    new commit

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+console.log("MONGODB_URI:", process.env.MONGODB_URI); // Debug statement
+
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
